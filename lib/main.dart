60e9e172c03f72f543c7c5fe716cb4c22360dae1
bharathi-mi <bharathi@urumee.com>
2023-04-12 09:45:18 +0530

    color theme changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'color_schemes.g.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,7 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
