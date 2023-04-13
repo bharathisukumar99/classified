3e9e0f9c6170e8a2d14bb6e4a78110ce6d5e1e85
bharathisukumar99@gmail.com <bharathi@millionitechnology.com>
2023-04-13 15:54:42 +0530

    onboarding screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:classified/login/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'color_schemes.g.dart';
+import 'firebase_options.dart';
 import 'home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MainApp());
 }
 
@@ -13,9 +20,18 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Classified App',
-        theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
-        darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-        home: Home());
+      debugShowCheckedModeBanner: false,
+      title: 'Classified App',
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme,
+          fontFamily: 'Raleway'),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/login': (context) => const Login(),
+      },
+    );
   }
 }
--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(),
+              SvgPicture.asset('assets/images/onboarding-1.svg',height: MediaQuery.of(context).size.height/3),
+              const Text('Find your home in the UAE',style: TextStyle(fontWeight: FontWeight.w600,color: Color(0xff1C1E1D)),textAlign: TextAlign.center,textScaleFactor: 1.7,),
+             const Text("Explore over 20,000 homes and find exactly what you're looking for with the help of 1000+ trusted agents .",style: TextStyle(fontWeight: FontWeight.w300,color: Colors.grey),textAlign: TextAlign.center,textScaleFactor: 0.8,),
+             const Spacer(),
+              FilledButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: FilledButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                  textScaleFactor: 1.2,
+                ),
+              )
+            ]),
+      ),
+    );
   }
 }
